4b5c138f660bd00e2f868f476689cb9d5accf155
BigCookie233 <86756026+BigCookie233@users.noreply.github.com>
2024-04-16 11:36:09 +0800

    Initial commit

---
+++ src/test/java/io/github/bigcookie233/simpleshop/SimpleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bigcookie233.simpleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
